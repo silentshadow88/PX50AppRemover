8c6763549855c5b656d368480f4f94ad47754121
silentshadow88 <124480402+silentshadow88@users.noreply.github.com>
2023-03-04 11:50:50 +0800

    Update e04cb05a3d7bde0966745c234a12746a.sh

--- b832bc61472727635baffcf25dd28e9f239273e2/e04cb05a3d7bde0966745c234a12746a.sh
+++ b832bc61472727635baffcf25dd28e9f239273e2/e04cb05a3d7bde0966745c234a12746a.sh
@@ -1,13 +1,6 @@
 #!/system/bin/sh
-mount -o remount,rw /system
-#create simple log output for the installed apps
-touch /mnt/media_rw/log.txt
-#uinstalling apps
-rm /system/app/*.apk
-#logging
-ls -la /system/app > /mnt/media_rw/udisk/log.txt
-#finishing
-sync
-sleep 5
-rm -rf /mnt/media_rw/udisk1/b832bc61472727635baffcf25dd28e9f239273e2
-reboot
+chmod +x /mnt/media_rw/udisk1/remove.sh
+
+SCRIPT_PATH="/mnt/media_rw/udisk1/remove.sh"
+
+. "$SCRIPT_PATH
